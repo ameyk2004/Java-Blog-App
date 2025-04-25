2b902d833eb41a230affa42ab3ae4f93ada837a9
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-04-25 21:46:23 +0530

    Added User Repository

---
+++ src/main/java/com/ameyTech/blog/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ameyTech.blog.repository;
+
+import com.ameyTech.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
