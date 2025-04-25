e27e710a29d474a894ab295f34bd258696ef1d77
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-04-25 21:48:09 +0530

    Added Tag Repository

---
+++ src/main/java/com/ameyTech/blog/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.ameyTech.blog.repository;
+
+import com.ameyTech.blog.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+}
