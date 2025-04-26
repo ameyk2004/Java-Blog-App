4b869a1e249a7fbefac11b6fa2c5cd4766162544
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-04-26 13:49:49 +0530

    Added Custom Error Handling

---
+++ src/main/java/com/ameyTech/blog/controller/ErrorController.java
@@ -0,0 +1,46 @@
+package com.ameyTech.blog.controller;
+
+import com.ameyTech.blog.domain.dto.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex){
+        log.error("Caught exception", ex);
+
+        ApiErrorResponse errorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An Unexpected Error Occurred")
+                .build();
+
+        return new ResponseEntity<>(
+                errorResponse,
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex){
+        log.error("Caught Illegal Argument exception", ex);
+
+        ApiErrorResponse errorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(
+                errorResponse,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+}
